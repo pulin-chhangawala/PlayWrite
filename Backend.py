2a11ab606f755f8e0f6c963dafeb0d4bc3cf0e1a
Pulin C <pulinchhangawala@gmail.com>
2024-09-23 18:44:22 -0400

    Working model and frontend. Re-organization of UI and addition of certain features yet to be completed

--- Backend.py
+++ Backend.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 from flask import Flask, request, jsonify, send_from_directory
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
@@ -6,7 +9,8 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='')
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -27,6 +31,7 @@ def create_model(learning_rate, activation, regularization, reg_rate):
 def serve_index():
     return send_from_directory('.', 'index.html')
 
+@app.route('/train', methods=['POST'])
 def train_model():
     data = request.json
     learning_rate = data['learning_rate']
@@ -40,9 +45,13 @@ def train_model():
     x_val_split, y_val_split = x_train[split_index:], y_train[split_index:]
 
     model = create_model(learning_rate, activation, regularization, reg_rate)
-    history = model.fit(x_train_split, y_train_split, epochs=5, validation_data=(x_val_split, y_val_split))
+    history = model.fit(x_train_split, y_train_split, epochs=15, validation_data=(x_val_split, y_val_split))
 
-    return jsonify({'accuracy': history.history['accuracy'], 'val_accuracy': history.history['val_accuracy']})
+    # Truncate accuracy values to 5 decimal places
+    accuracy = [round(acc, 5) for acc in history.history['accuracy']]
+    val_accuracy = [round(val_acc, 5) for val_acc in history.history['val_accuracy']]
+
+    return jsonify({'accuracy': accuracy, 'val_accuracy': val_accuracy})
 
 if __name__ == '__main__':
     app.run(debug=True)
